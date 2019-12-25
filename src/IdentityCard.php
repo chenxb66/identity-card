b61b9799b1e49e8f4ea4cf2a2d60bd0d94792b9f
chenxb <chenxb@qcplay2.com>
2019-12-25 20:01:11 +0800

    修正地区错误

--- src/IdentityCard.php
+++ src/IdentityCard.php
@@ -99,7 +99,7 @@ class IdentityCard
             11, 12, 13, 14, 15, 21, 22, 23, 31, 32, 33, 34, 35, 36, 37, 41, 42,
             43, 44, 45, 46, 50, 51, 52, 53, 54, 61, 62, 63, 64, 65, 71, 81, 82, 91
         ];
-        if (!array_search(substr($idCard, 0, 2), $area)) {
+        if (array_search(substr($idCard, 0, 2), $area) === false) {
             return false;
         }
 
